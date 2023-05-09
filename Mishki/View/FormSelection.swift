e900466267f659444fab0d5fa83b58e9b5f34018
aym183 <ayman.ali1302@gmail.com>
2023-05-09 07:33:52 +0530

    added FormSelection view and nav to it

---
+++ Mishki/View/FormSelection.swift
@@ -0,0 +1,20 @@
+//
+//  FormSelection.swift
+//  Mishki
+//
+//  Created by Ayman Ali on 09/05/2023.
+//
+
+import SwiftUI
+
+struct FormSelection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FormSelection_Previews: PreviewProvider {
+    static var previews: some View {
+        FormSelection()
+    }
+}
--- Mishki/View/HomePage.swift
+++ Mishki/View/HomePage.swift
@@ -9,69 +9,77 @@ import SwiftUI
 
 struct HomePage: View {
     @State var productsListed = false
+    @State var formShown = false
     var body: some View {
         GeometryReader { geometry in
-            ZStack {
-                Color(.white).ignoresSafeArea()
-                VStack {
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("mishki.shop/alicap").font(Font.system(size: 20)).fontWeight(.bold)
-                            
-                            HStack(spacing: 25) {
-                                Button(action: {}) {
-                                    Image(systemName: "doc").background(
-                                        Circle().fill(.gray).frame(width: 28, height: 28).opacity(0.2)
-                                    )
+            NavigationStack {
+                ZStack {
+                    Color(.white).ignoresSafeArea()
+                    VStack {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("mishki.shop/alicap").font(Font.system(size: 20)).fontWeight(.bold)
+                                
+                                HStack(spacing: 25) {
+                                    Button(action: {}) {
+                                        Image(systemName: "doc").background(
+                                            Circle().fill(.gray).frame(width: 28, height: 28).opacity(0.2)
+                                        )
+                                    }
+                                    
+                                    Button(action: {}) {
+                                        Image(systemName: "arrow.up.forward").background(
+                                            Circle().fill(.gray).frame(width: 28, height: 28).opacity(0.2)
+                                        )
+                                    }
                                 }
-                               
-                                Button(action: {}) {
-                                    Image(systemName: "arrow.up.forward").background(
-                                        Circle().fill(.gray).frame(width: 28, height: 28).opacity(0.2)
-                                    )
-                                }
+                                .foregroundColor(.black)
+                                .font(Font.system(size: 13))
+                                .padding(.leading, 10).padding(.top, 0.5)
+                                .fontWeight(.bold)
+                                
                             }
-                            .foregroundColor(.black)
-                            .font(Font.system(size: 13))
-                            .padding(.leading, 10).padding(.top, 0.5)
-                            .fontWeight(.bold)
+                            Spacer()
                             
+                            Button(action: {}) {
+                                Image(systemName: "person.circle").font(Font.system(size: 60)).foregroundColor(.black)
+                            }
                         }
-                        Spacer()
+                        .padding(.horizontal, 10)
                         
-                        Button(action: {}) {
-                            Image(systemName: "person.circle").font(Font.system(size: 60)).foregroundColor(.black)
-                        }
-                    }
-                    .padding(.horizontal, 10)
-                    
-                    HStack {
-                        Button(action: {}) {
-                            HStack(spacing: 6) {
-                                Image(systemName: "plus")
-                                Text("Add")
+                        HStack {
+                            Button(action: { formShown.toggle() }) {
+                                HStack(spacing: 6) {
+                                    Image(systemName: "plus")
+                                    Text("Add")
+                                }
+                                .frame(width: 85, height: 35).padding(4).background(.black).foregroundColor(.white).cornerRadius(10).font(Font.system(size: 20)).fontWeight(.heavy)
                             }
-                            .frame(width: 85, height: 35).padding(4).background(.black).foregroundColor(.white).cornerRadius(10).font(Font.system(size: 20)).fontWeight(.heavy)
-                        }
-                        Spacer()
-                    }
-                    .padding(.horizontal, 10).padding(.top)
-                    
-                    Spacer()
-                    
-                    if !productsListed {
-                        Text("No products or links added yet.").fontWeight(.semibold)
-                        Spacer()
-                    } else {
-                        ScrollView {
-                            Text("No products.").fontWeight(.semibold)
+                            
                             Spacer()
                         }
-                        .border(.black, width: 2).padding(.top)
+                        .padding(.horizontal, 10).padding(.top)
+                        .navigationDestination(isPresented: $formShown) {
+                            FormSelection()
+                        }
                         
+                        
+                        Spacer()
+                        
+                        if !productsListed {
+                            Text("No products or links added yet.").fontWeight(.semibold)
+                            Spacer()
+                        } else {
+                            ScrollView {
+                                Text("No products.").fontWeight(.semibold)
+                                Spacer()
+                            }
+                            .border(.black, width: 2).padding(.top)
+                            
+                        }
                     }
+                    .frame(width: geometry.size.width-40, height: geometry.size.height-20)
                 }
-                .frame(width: geometry.size.width-40, height: geometry.size.height-20)
             }
         }
     }
