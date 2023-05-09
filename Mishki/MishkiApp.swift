d70461210957dfe719cfa5db120894636b110bf4
aym183 <ayman.ali1302@gmail.com>
2023-05-09 06:22:39 +0530

    Initial Commit

---
+++ Mishki/MishkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MishkiApp.swift
+//  Mishki
+//
+//  Created by Ayman Ali on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MishkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
