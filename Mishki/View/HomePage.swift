f6a490ede29a36753688bb75f58db7037c08d42d
aym183 <ayman.ali1302@gmail.com>
2023-05-09 07:19:43 +0530

    created home page

--- Mishki/View/HomePage.swift
+++ Mishki/View/HomePage.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct HomePage: View {
+    @State var productsListed = false
     var body: some View {
-        VStack {
-            Image(systemName: "globessdsss")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            ZStack {
+                Color(.white).ignoresSafeArea()
+                VStack {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("mishki.shop/alicap").font(Font.system(size: 20)).fontWeight(.bold)
+                            
+                            HStack(spacing: 25) {
+                                Button(action: {}) {
+                                    Image(systemName: "doc").background(
+                                        Circle().fill(.gray).frame(width: 28, height: 28).opacity(0.2)
+                                    )
+                                }
+                               
+                                Button(action: {}) {
+                                    Image(systemName: "arrow.up.forward").background(
+                                        Circle().fill(.gray).frame(width: 28, height: 28).opacity(0.2)
+                                    )
+                                }
+                            }
+                            .foregroundColor(.black)
+                            .font(Font.system(size: 13))
+                            .padding(.leading, 10).padding(.top, 0.5)
+                            .fontWeight(.bold)
+                            
+                        }
+                        Spacer()
+                        
+                        Button(action: {}) {
+                            Image(systemName: "person.circle").font(Font.system(size: 60)).foregroundColor(.black)
+                        }
+                    }
+                    .padding(.horizontal, 10)
+                    
+                    HStack {
+                        Button(action: {}) {
+                            HStack(spacing: 6) {
+                                Image(systemName: "plus")
+                                Text("Add")
+                            }
+                            .frame(width: 85, height: 35).padding(4).background(.black).foregroundColor(.white).cornerRadius(10).font(Font.system(size: 20)).fontWeight(.heavy)
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal, 10).padding(.top)
+                    
+                    Spacer()
+                    
+                    if !productsListed {
+                        Text("No products or links added yet.").fontWeight(.semibold)
+                        
+                        Spacer()
+                    }
+                }
+                .frame(width: geometry.size.width-40, height: geometry.size.height-20)
+            }
         }
-        .padding()
     }
 }
 
