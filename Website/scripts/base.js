389dbd0cae5648c56ed45247efbd088f1715734c
aym183 <ayman.ali1302@gmail.com>
2023-05-16 17:55:03 +0530

    Added product fetching from firebase

--- Website/scripts/base.js
+++ Website/scripts/base.js
@@ -14,7 +14,8 @@ function getLinks() {
     )
     .then(response => response.json())
     .then(data => {
-        var productsList = document.getElementsByClassName("links-list")[0];
+        console.log(data)
+        var linksList = document.getElementsByClassName("links-list")[0];
         for (var i = 0; i < data.links.length; i++) {
             var a = document.createElement("a");
             a.setAttribute("href", data.links[i][1]);
@@ -24,7 +25,40 @@ function getLinks() {
             p.innerHTML = data.links[i][0];
             div.appendChild(p);
             a.appendChild(div);
-            productsList.appendChild(a);
+            linksList.appendChild(a);
+        }
+
+        var productsList = document.getElementsByClassName("products-list")[0];
+        for (var i = 0; i < data.products.length; i++) {
+            var product = document.createElement("div");
+            product.setAttribute("class", "products");
+
+            var img = document.createElement("img");
+            img.setAttribute("src", data.products[i][3]);
+            img.setAttribute("class", "product-image");
+            img.setAttribute("alt", "your image description");
+            product.appendChild(img);
+
+            var p = document.createElement("p");
+            p.setAttribute("class", "product-title");
+            p.innerHTML = data.products[i][0];
+            product.appendChild(p);
+
+            var div = document.createElement("div");
+            div.setAttribute("class", "product-horizontal-row");
+
+            var price = document.createElement("p");
+            price.innerHTML = `<b>${data.products[i][2]} AED</b>`;
+            div.appendChild(price);
+
+            var button = document.createElement("button");
+            button.setAttribute("type", "button");
+            button.innerHTML = "Buy Now";
+            div.appendChild(button);
+
+            product.appendChild(div);
+
+            productsList.appendChild(product);
         }
     })
     .catch(error => console.error(error));
